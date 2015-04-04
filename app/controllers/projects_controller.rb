45ad57c0e3e0808ccf02331bf1634c6a24d093be
Den <denis.notin@gmail.com>
2015-04-04 15:20:50 +0300

    first commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,40 @@
+class ProjectsController < ApplicationController
+
+	http_basic_authenticate_with name: "den", password: "secret", except: [:index, :show]
+
+	def index
+		@projects = Project.all
+	end
+
+	def create
+		@project = Project.new(project_params)
+
+		if @project.save
+			redirect_to @project
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@project = Project.find(params[:id])
+
+		if @project.update(project_params)
+			redirect_to @project
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project = Project.find(params[:id])
+		@project.destroy
+
+		redirect_to projects_path
+	end
+
+	private
+		def project_params
+			params.require(:project).permit(:title)
+		end
+end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class TasksController < ApplicationController
+
+	http_basic_authenticate_with name: "den", password: "secret", only: :destroy
+
+	# def index
+	# 	@tasks = Task.all
+	# end
+
+	def create
+		@project = Project.find(params[:project_id])
+		@task = @project.tasks.create(task_params)
+		redirect_to project_path(@project)
+	end
+
+	def destroy
+		@project = Project.find(params[:project_id])
+		@task = @project.tasks.find(params[:id])
+		@task.destroy
+		redirect_to project_path(@project)
+	end
+
+	private
+		def task_params
+			params.require(:task).permit(:task, :date)
+		end
+end
