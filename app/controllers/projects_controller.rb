eea14bbabe0e931e11b17c9902fc205e67e8bc67
Den <denis.notin@gmail.com>
2015-06-18 22:43:56 +0300

    commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,36 +1,36 @@
 class ProjectsController < ApplicationController
   respond_to :html, :js
 
-	def index
-		@projects = current_user.projects
-		@project = Project.new
-		@task = Task.new
-	end
+  def index
+    @projects = current_user.projects
+    @project = Project.new
+    @task = Task.new
+  end
 
-	def create
-		@project = current_user.projects.new(project_params)
-		@projects = current_user.projects
-		@task = Task.new
-		unless @project.save
+  def create
+    @project = current_user.projects.new(project_params)
+    @projects = current_user.projects
+    @task = Task.new
+    unless @project.save
       render :nothing => true
     end
-	end
+  end
 
-	def update
-		@project = current_user.projects.find(params[:id])
-		@projects = current_user.projects
-		@project.update(project_params)
-		@task = @project.tasks.new
-		@project.save
-	end
+  def update
+    @project = current_user.projects.find(params[:id])
+    @projects = current_user.projects
+    @project.update(project_params)
+    @task = @project.tasks.new
+    @project.save
+  end
 
-	def destroy
-		@project = current_user.projects.find(params[:id])
-		@project.destroy
-	end
+  def destroy
+    @project = current_user.projects.find(params[:id])
+    @project.destroy
+  end
 
-	private
-		def project_params
-			params.require(:project).permit(:title)
-		end
+  private
+    def project_params
+      params.require(:project).permit(:title)
+    end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,22 @@
 class TasksController < ApplicationController
-	respond_to :html, :js
+  respond_to :html, :js
 
-	def create
-		@project = current_user.projects.find(params[:project_id])
-		@task = @project.tasks.create(task_params)
+  def create
+    @project = current_user.projects.find(params[:project_id])
+    @task = @project.tasks.create(task_params)
     unless @task.save
       render :nothing => true
     end
-	end
+  end
 
-	def update
-		@project = current_user.projects.find(params[:project_id])
-		@task = @project.tasks.find(params[:id])
-		@task.update(task_params)
-	end
+  def update
+    @project = current_user.projects.find(params[:project_id])
+    @task = @project.tasks.find(params[:id])
+    @task.update(task_params)
+  end
 
-	def sorting
-		@project = current_user.projects.find(params[:project_id])
+  def sorting
+    @project = current_user.projects.find(params[:project_id])
     @project.tasks.each do | task |
       task.position = params[:task].index(task.id.to_s)+1
       task.save
@@ -24,14 +24,14 @@ class TasksController < ApplicationController
     render nothing: true
   end
 
-	def destroy
-		@project = current_user.projects.find(params[:project_id])
-		@task = @project.tasks.find(params[:id])
-		@task.destroy
-	end
+  def destroy
+    @project = current_user.projects.find(params[:project_id])
+    @task = @project.tasks.find(params[:id])
+    @task.destroy
+  end
 
-	private
-		def task_params
-			params.require(:task).permit(:title, :date, :status)
-		end
+  private
+    def task_params
+      params.require(:task).permit(:title, :date, :status)
+    end
 end
