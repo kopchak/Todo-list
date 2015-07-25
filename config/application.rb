93b0b7925a485fb47d521b972b1ba8762a86cfb3
Den <denis.notin@gmail.com>
2015-07-25 22:44:00 +0300

    enable assets, edit config/production.rb

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Tdlist
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.serve_static_assets = true
     config.generators do |g|
         g.test_framework :rspec, fixtures: true, views: false
         g.fixture_replacement :factory_girl, dir: 'spec/factories'
